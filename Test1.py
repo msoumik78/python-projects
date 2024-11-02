f040ce37319d1f3ce8374490a4308039af49852f
Soumik Mukherjee <msoumik78@gmail.com>
2024-11-02 20:19:25 +0530

    first commit

---
+++ Test1.py
@@ -0,0 +1,11 @@
+#This is just a sample program
+x = 2
+while True:
+    #x=1
+    print("my name is sayantani")
+    print("Hello world")
+    x = x+1
+    x = 1
+    #if x == 100:
+    break
+print("Now time to quit")
